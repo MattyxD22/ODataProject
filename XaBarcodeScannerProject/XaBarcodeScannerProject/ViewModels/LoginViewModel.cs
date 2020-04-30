80f4cdc3ec251e1c7074877239cad92eb119c373
Mathias Christensen <mathiaschristensen2009@live.dk>
2020-04-30 13:37:58 +0200

    loginviewmodel har mvvm

--- XaBarcodeScannerProject/XaBarcodeScannerProject/ViewModels/LoginViewModel.cs
+++ XaBarcodeScannerProject/XaBarcodeScannerProject/ViewModels/LoginViewModel.cs
@@ -1,10 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using XaBarcodeScannerProject.Views;
+using Xamarin.Forms;
 
-namespace XaBarcodeScannerProject.ViewModels
+namespace XaBarcodeScannerProject.ViewModels 
 {
-    class LoginViewModel
-    {
+    class LoginViewModel : BaseViewModel
+	{
+		private string userEmail;
+
+		public string UserEmail
+		{
+			get { return userEmail; }
+			set { userEmail = value; OnPropertyChanged(); }
+		}
+
+		private string userPassword;
+
+		public string UserPassword
+		{
+			get { return userPassword; }
+			set { userPassword = value; OnPropertyChanged(); }
+		}
+
+        public Command GetTextCommand => new Command(async () =>
+        {
+			UserEmail = 
+			Console.WriteLine();
+
+        });
+
+
     }
 }
--- XaBarcodeScannerProject/XaBarcodeScannerProject/Views/LoginView.xaml.cs
+++ XaBarcodeScannerProject/XaBarcodeScannerProject/Views/LoginView.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using XaBarcodeScannerProject.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,7 @@ namespace XaBarcodeScannerProject.Views
         public LoginView()
         {
             InitializeComponent();
+            BindingContext = new LoginViewModel();
         }
     }
 }
