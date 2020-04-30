ed1cf7dc6e98d9c27100c2bfeae813918510dad6
MartinIversen8 <martin-iversen@hotmail.com>
2020-04-30 12:38:26 +0200

    mvvm og mere

---
+++ XaBarcodeScannerProject/XaBarcodeScannerProject/ViewModels/ScannerPageViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using XaBarcodeScannerProject.Views;
+
+namespace XaBarcodeScannerProject.ViewModels
+{
+   public class ScannerPageViewModel : BaseViewModel
+    {
+        private string scannedCode;
+
+        public string ScannedCode
+        {
+            get { return scannedCode; }
+            set 
+            { scannedCode = value; 
+              
+            }
+        }
+
+        public Command Scan => new Command(async () =>
+        {
+           
+
+        });
+
+
+    }
+}
