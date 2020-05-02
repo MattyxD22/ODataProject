6575210fe8fd764e447c7ece4ee0e7c0900ea7cd
Mathias Christensen <mathiaschristensen2009@live.dk>
2020-05-02 14:16:32 +0200

    fjernede projekter som ikke var slettet rigtigt

--- NavisionWebSite/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace NavisionWebsite.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- NavisionWebSite/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace NavisionWebsite.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
